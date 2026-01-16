988e65429de48ac99042a76870194b11045581fc
jeugfer600 <jeugfer600@g.educaand.es>
2026-01-16 12:14:25 +0100

    solución problema Mockito

--- src/test/java/ControladorIluminacionTest.java
+++ src/test/java/ControladorIluminacionTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.mockito.Mockito.*;
 
 class ControladorIluminacionTest {
@@ -17,5 +19,19 @@ class ControladorIluminacionTest {
 
         // Creamos la instancia real usando los mocks
         controlador = new ControladorIluminacion(mockSensor, mockReloj, mockInterruptor);
+    }
+
+    @Test
+    void testEncenderCuandoEsDeNocheYEstaOscuro() {
+        // 1. Configuración (Stubbing): Forzamos la realidad
+        when(mockSensor.obtenerLux()).thenReturn(10); // Muy oscuro
+        when(mockReloj.obtenerHora()).thenReturn(22);  // 10:00 PM
+
+        // 2. Ejecución
+        controlador.actualizarEstado();
+
+        // 3. Verificación: ¿Se llamó al método encender()?
+        verify(mockInterruptor).encender();
+        verify(mockInterruptor, never()).apagar();
     }
 }
